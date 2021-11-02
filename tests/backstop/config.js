a6deb375e9c607ed5cd7089d4f39563875f4a8da
Tom Rees-Herdman <tom@techanvil.co.uk>
2021-11-02 21:40:36 +0000

    Dump shm-size mk2.

--- tests/backstop/config.js
+++ tests/backstop/config.js
@@ -38,8 +38,8 @@ module.exports = {
 	// Use a custom command template to make sure it works correctly in the GitHub actions environment.
 	// The only difference between the original dockerCommandTemplate and this one is that we use --tty flag
 	// in the current template only if it is supported by the current STDOUT stream.
-	dockerCommandTemplate: 'docker run --rm -it ubuntu df -h | grep shm',
-	// dockerCommandTemplate: `docker run --rm -i${
+	dockerCommandTemplate: 'docker run --rm -it ubuntu df -h',
+	// dockerCommandTemplate: `docker run --shm-size=2G --rm -i${
 	// 	process.stdout.isTTY ? ' --tty' : ''
 	// } --mount type=bind,source="{cwd}",target=/src backstopjs/backstopjs:{version} {backstopCommand} {args}`,
 	engine: 'puppeteer',
