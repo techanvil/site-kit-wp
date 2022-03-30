d45b394258004939c114e228e12cd7dfd4169004
Tom Rees-Herdman <tom@techanvil.co.uk>
2022-03-30 12:52:27 +0100

    Attempt to retrieve more error info from console log.

--- tests/e2e/config/bootstrap.js
+++ tests/e2e/config/bootstrap.js
@@ -131,7 +131,7 @@ function removePageEvents() {
  * @since 1.0.0
  */
 function observeConsoleLogging() {
-	page.on( 'console', ( message ) => {
+	page.on( 'console', async ( message ) => {
 		const type = message.type();
 		if ( ! OBSERVED_CONSOLE_MESSAGE_TYPES.hasOwnProperty( type ) ) {
 			return;
@@ -184,6 +184,7 @@ function observeConsoleLogging() {
 			return;
 		}
 
+		// HERE?
 		// Some error messages which don't impact test results can
 		// be safely ignored.
 		if (
@@ -229,13 +230,25 @@ function observeConsoleLogging() {
 			text
 		);
 
+		const args = await Promise.all(
+			message.args().map( ( messageArg ) =>
+				messageArg.executionContext().evaluate( ( arg ) => {
+					// I'm in a page context now. If my arg is an error - get me its message.
+					if ( arg instanceof Error ) return arg.message;
+					// return arg right away. since we use `executionContext.evaluate`, it'll return JSON value of
+					// the argument if possible, or `undefined` if it fails to stringify it.
+					return arg;
+				}, messageArg )
+			)
+		);
+
 		// Disable reason: We intentionally bubble up the console message
 		// which, unless the test explicitly anticipates the logging via
 		// @wordpress/jest-console matchers, will cause the intended test
 		// failure.
 
 		// eslint-disable-next-line no-console
-		console[ logFunction ]( message );
+		console[ logFunction ]( args );
 		// console[ logFunction ]( text );
 	} );
 }
