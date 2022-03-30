2d28ecd19e51c49f19e8141d6dca2125336e209d
Tom Rees-Herdman <tom@techanvil.co.uk>
2022-03-30 14:37:24 +0100

    Revert console.log inspection.

--- tests/e2e/config/bootstrap.js
+++ tests/e2e/config/bootstrap.js
@@ -131,7 +131,7 @@ function removePageEvents() {
  * @since 1.0.0
  */
 function observeConsoleLogging() {
-	page.on( 'console', async ( message ) => {
+	page.on( 'console', ( message ) => {
 		const type = message.type();
 		if ( ! OBSERVED_CONSOLE_MESSAGE_TYPES.hasOwnProperty( type ) ) {
 			return;
@@ -230,17 +230,17 @@ function observeConsoleLogging() {
 			text
 		);
 
-		const args = await Promise.all(
-			message.args().map( ( messageArg ) =>
-				messageArg.executionContext().evaluate( ( arg ) => {
-					// I'm in a page context now. If my arg is an error - get me its message.
-					if ( arg instanceof Error ) return arg.message;
-					// return arg right away. since we use `executionContext.evaluate`, it'll return JSON value of
-					// the argument if possible, or `undefined` if it fails to stringify it.
-					return arg;
-				}, messageArg )
-			)
-		);
+		// const args = await Promise.all(
+		// 	message.args().map( ( messageArg ) =>
+		// 		messageArg.executionContext().evaluate( ( arg ) => {
+		// 			// I'm in a page context now. If my arg is an error - get me its message.
+		// 			if ( arg instanceof Error ) return arg.message;
+		// 			// return arg right away. since we use `executionContext.evaluate`, it'll return JSON value of
+		// 			// the argument if possible, or `undefined` if it fails to stringify it.
+		// 			return arg;
+		// 		}, messageArg )
+		// 	)
+		// );
 
 		// Disable reason: We intentionally bubble up the console message
 		// which, unless the test explicitly anticipates the logging via
@@ -248,7 +248,7 @@ function observeConsoleLogging() {
 		// failure.
 
 		// eslint-disable-next-line no-console
-		console[ logFunction ]( args );
+		console[ logFunction ]( message );
 		// console[ logFunction ]( text );
 	} );
 }
