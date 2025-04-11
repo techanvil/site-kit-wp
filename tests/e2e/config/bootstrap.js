64272eb9bea54cb808a56ba008e68bbe51687240
Tom Rees-Herdman <tom.rees-herdman@10up.com>
2025-04-11 14:18:04 +0100

    Add a separate env var for debug log prefix matching.

--- tests/e2e/config/bootstrap.js
+++ tests/e2e/config/bootstrap.js
@@ -138,13 +138,17 @@ function removePageEvents() {
  */
 export function shouldLog() {
 	const targetTest = process.env.RETRY_FULL_TEST_NAME;
+	const debugLogTestPrefix = process.env.DEBUG_LOG_TEST_PREFIX;
 	// `currentTestName` is the full test name (including describe names)
 	const currentTest = expect.getState().currentTestName;
 
 	// Log if we're not targeting a specific test, or if we're in the target test
 	// return ! targetTest || targetTest === currentTest;
 	// Modded to allow specifying a test prefix to match:
-	return ! targetTest || currentTest?.startsWith( targetTest );
+	return (
+		! targetTest ||
+		currentTest?.startsWith( debugLogTestPrefix || targetTest )
+	);
 }
 
 /**
